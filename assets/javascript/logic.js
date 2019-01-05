713a2098fe48dabedc9a8daad76ba0927513319c
Noah A <nanderson815@gmail.com>
2019-01-05 10:04:16 -0500

    Updated 1 5 19

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,9 +1,12 @@
-$(document).ready(function () {
-
-$('h1').click(function (){
-    console.log("This works!")
-});
 
 
-
-});
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCMPVbRG5PFeB4pHSaEvzpMywGCaxJ2I74",
+    authDomain: "rock-paper-scissors-83540.firebaseapp.com",
+    databaseURL: "https://rock-paper-scissors-83540.firebaseio.com",
+    projectId: "rock-paper-scissors-83540",
+    storageBucket: "",
+    messagingSenderId: "318063060937"
+};
+firebase.initializeApp(config);
